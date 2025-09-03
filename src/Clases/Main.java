874f5683ac4bb1b6f63df1580211f02103156cea
Elpapu3 <166446580+Elpapu3@users.noreply.github.com>
2025-09-03 19:57:06 -0300

    Update Main.java

--- src/Clases/Main.java
+++ src/Clases/Main.java
@@ -2,20 +2,16 @@ package Clases;
 
 import javax.swing.*;
 
-/**
- * Clase principal del juego.
- * Esta es la ventana que abre el juego y muestra el "Tablero".
- */
 public class Main extends JFrame {
 
     public Main() {
-        setTitle("Space Invader"); // Título de la ventana
-        setDefaultCloseOperation(EXIT_ON_CLOSE); // Cierra el juego al apretar la X
-        Tablero panel = new Tablero(); // Creamos el panel donde está el juego
-        setContentPane(panel); // Lo agregamos a la ventana
-        pack(); // Ajusta el tamaño al del panel
-        setLocationRelativeTo(null); // Centra la ventana en la pantalla
-        setVisible(true); // Hace visible la ventana
+        setTitle("Space Invader"); 
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Tablero panel = new Tablero(); 
+        setContentPane(panel); 
+        pack(); 
+        setLocationRelativeTo(null); 
+        setVisible(true); 
     }
 
     public static void main(String[] args) {
